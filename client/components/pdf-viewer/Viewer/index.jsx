2dae99028a69ae7f63c135cac9462e49a567fb08
Haris <harisahmad.188@gmail.com>
2021-04-17 00:53:43 -0400

    pdf visible now

--- client/components/pdf-viewer/Viewer/index.jsx
+++ client/components/pdf-viewer/Viewer/index.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { Container } from '@material-ui/core';
+import React, { useEffect, useState } from 'react'
+import  {pdfjs, Document, Page } from 'react-pdf';
+import InfiniteScroll from 'react-infinite-scroll-component'
+import VisibilitySensor from 'react-visibility-sensor'
 
-const Viewer = ()=>{
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const Viewer = (props)=>{
+    const initItems = new Array(2)
+    initItems.fill((props)=><Page 
+    pageIndex={0}
+/>)
+
+    const [items, setItems] = useState(initItems)
+
+    const callNext = ()=>{
+        const newItems =  new Array(10)
+        newItems.fill((props)=><Page 
+        pageIndex={0}
+    />)
+        
+        const xItems = [...items, ...newItems]
+        setItems(xItems)
+    }
+
     return (
-        <div>hello</div>
+        <div>
+            <Container>
+                <Document
+                    file={props.fileLink}
+                    onLoadSuccess={()=>{console.log('loaded')}}
+                    onLoadError={(e)=>{console.log(e)}}
+                >
+                   <InfiniteScroll
+                        dataLength={items.length}
+                        next={callNext}
+                        hasMore={true}
+                        loader={<div>Loding...</div>}
+                   >
+                        {items.map((Item, i)=>(
+                            <VisibilitySensor
+                                key={i}
+                                onChange={(isVisible)=>{
+                                    console.log('visible',i)
+                                }}
+                            >
+                                <Item />
+                            </VisibilitySensor>
+                        ))}
+                   </InfiniteScroll>
+                </Document>
+            </Container>
+        </div>
     )
 }
 
