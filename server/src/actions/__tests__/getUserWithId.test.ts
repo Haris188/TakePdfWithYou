2f2dd1c7d656896e158e7b714a6ee1ab763f43c2
Haris <harisahmad.188@gmail.com>
2021-04-30 20:50:57 -0400

    new

--- server/src/actions/__tests__/getUserWithId.test.ts
+++ server/src/actions/__tests__/getUserWithId.test.ts
@@ -2,7 +2,7 @@ import actions from "..";
 
 describe('actions.getUserWithId', () => {
     it('should get user with provided Id', async () => {
-        const res = await actions.getUserWithId('1')
+        const res:any = await actions.getUserWithId('1')
         expect(res.id).toBe('1')
     });
 });
--- server/src/entities/FileAccess/index.ts
+++ server/src/entities/FileAccess/index.ts
@@ -4,7 +4,7 @@ import TestFileAccess from './TestFileAccess'
 import FirestoreAccess from './FirestoreAccess'
 
 dotenv.config()
-const fileAccess = process.env.NODE_ENV ==='test'
+const fileAccess:any = process.env.NODE_ENV ==='test'
 ? TestFileAccess
 : FirestoreAccess
 
