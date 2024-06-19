0adb5134a3d3507c39c36b89a6424e274ec7f29c
Haris Ahmad <40364018+Haris188@users.noreply.github.com>
2024-06-19 12:48:27 -0400

    merge to main (#9)
    * build changed
    
    * build setting changed
    
    * build setting changed
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * added S3
    
    * dockerized
    
    ---------
    
    Co-authored-by: = <=>

--- server/src/entities/User/mockUserData.ts
+++ server/src/entities/User/mockUserData.ts
@@ -6,7 +6,7 @@ export const def = {
     password: '23423423',
     pdfs:[{
         id: '1',
-        downloadLink: 'download ling',
+        downloadLink: 'https://bankcapy.s3.ca-central-1.amazonaws.com/1/InnPower%20__%20Select%20My%20Account.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIATNZTULDWV6EKCEX4%2F20240618%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20240618T013254Z&X-Amz-Expires=3600&X-Amz-Signature=6d9139e997f358ff3ddd2b902e8ca32082e191116494b86c3cb9f6237e1d8bbd&X-Amz-SignedHeaders=host&x-id=GetObject',
         bookmark: '1'
     }]
 }
